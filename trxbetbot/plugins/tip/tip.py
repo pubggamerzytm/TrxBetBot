6ae24f5a2e176e7eae1937cb5500296ec517fb9f
Endogen <081081@gmx.net>
2020-01-27 22:13:39 +0100

    Added possibility to tip user by replying to message

--- trxbetbot/plugins/tip/tip.py
+++ trxbetbot/plugins/tip/tip.py
@@ -21,36 +21,52 @@ class Tip(TrxBetBotPlugin):
     @TrxBetBotPlugin.threaded
     @TrxBetBotPlugin.send_typing
     def execute(self, bot, update, args):
-        if len(args) != 2:
-            update.message.reply_text(
-                text=f"Usage:\n{self.get_usage()}",
-                parse_mode=ParseMode.MARKDOWN)
-            return
+        reply = update.message.reply_to_message
 
-        amount = args[0]
+        # Tip the user that you reply to
+        if reply:
+            if len(args) != 1:
+                msg = f"{emo.ERROR} You are tipping the user you reply to. " \
+                      "Only allowed argument is the amount."
+                logging.error(f"{msg} - {update}")
+                update.message.reply_text(msg)
+                return
 
-        # Check if amount is valid
+            amount = args[0]
+            to_username = reply.from_user.username.replace("@", "")
+
+        # Tip user specified in message
+        else:
+            if len(args) != 2:
+                update.message.reply_text(
+                    text=f"Usage:\n{self.get_usage()}",
+                    parse_mode=ParseMode.MARKDOWN)
+                return
+
+            amount = args[0]
+            to_username = args[1].replace("@", "")
+
         try:
+            # Check if amount is valid
             float(amount)
         except:
             msg = f"{emo.ERROR} Provided amount is not valid"
-            logging.info(f"{msg} - {update}")
+            logging.error(f"{msg} - {update}")
             update.message.reply_text(msg)
             return
-
-        to_username = args[1].replace("@", "")
 
         sql = self.get_resource("select_user.sql")
         res = self.execute_global_sql(sql, to_username)
 
         if not res["success"]:
-            msg = f"Something went wrong. Please contact @Wikioshi the owner of this bot"
-            update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
+            msg = f"{emo.ERROR} Something went wrong. Please contact @HashLotto_Admin"
+            logging.error(f"{msg} - {update}")
+            update.message.reply_text(msg)
             return
 
         if not res["data"]:
             msg = f"{emo.ERROR} User @{to_username} doesn't have a wallet yet"
-            logging.info(f"{msg} - {update}")
+            logging.error(f"{msg} - {update}")
             update.message.reply_text(msg)
             return
 
@@ -65,8 +81,8 @@ class Tip(TrxBetBotPlugin):
         res = self.execute_global_sql(sql, from_user_id)
 
         if not res["success"]:
-            msg = f"Something went wrong. Please contact @Wikioshi the owner of this bot"
-            update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
+            msg = f"{emo.ERROR} Something went wrong. Please contact @HashLotto_Admin"
+            update.message.reply_text(msg)
             return
 
         data = res["data"]
@@ -74,7 +90,7 @@ class Tip(TrxBetBotPlugin):
         if not data:
             msg = f"{emo.ERROR} You don't have a wallet yet. " \
                   f"Create one by talking to the bot @{bot.name}"
-            logging.info(f"{msg} - {update}")
+            logging.error(f"{msg} - {update}")
             update.message.reply_text(msg)
             return
 
@@ -89,9 +105,9 @@ class Tip(TrxBetBotPlugin):
 
         # Check if address has enough balance
         if float(amount) > float(available_amount):
-            msg = f"{emo.ERROR} Not enough funds. Your balance is {available_amount} TRX"
-            logging.info(f"{msg} - {data[0][1]} - {update}")
-            update.message.reply_text(msg)
+            msg = f"{emo.ERROR} Not enough funds. Your balance is `{available_amount}` TRX"
+            update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
+            logging.error(f"{msg} - {data[0][1]} - {update}")
             return
 
         try:
@@ -117,13 +133,13 @@ class Tip(TrxBetBotPlugin):
                     # Tipping user has a username
                     bot.send_message(
                         to_user_id,
-                        f"You received `{amount}` TRX from @{from_username}",
+                        f"You received `{amount}` TRX from @{utl.esc_md(from_username)}",
                         parse_mode=ParseMode.MARKDOWN)
                 else:
                     # Tipping user doesn't have a username
                     bot.send_message(
                         to_user_id,
-                        f"You received `{amount}` TRX from @{from_firstname}",
+                        f"You received `{amount}` TRX from @{utl.esc_md(from_firstname)}",
                         parse_mode=ParseMode.MARKDOWN)
             except:
                 logging.info(f"User {to_username} ({to_user_id}) couldn't be notified about tip")
@@ -137,6 +153,7 @@ class Tip(TrxBetBotPlugin):
             if str(e) == "key 'transaction' not in send result":
                 msg = f"{emo.ERROR} Balance not sufficient. Try removing fee of `{con.TRX_FEE}` TRX"
                 update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
+                logging.error(f"{msg} - {update}")
             else:
                 update.message.reply_text(f"{emo.ERROR} {repr(e)}")
                 self.notify(e)
