396684773f03a1f6572f00c29e872a42443e0d9a
endogen <ramodroll>
2020-05-29 09:28:58 +0200

    Removed auto remove message functionality

--- trxbetbot/plugins/airdrop/airdrop.py
+++ trxbetbot/plugins/airdrop/airdrop.py
@@ -180,27 +180,4 @@ class Airdrop(TrxBetBotPlugin):
                 self.notify(msg)
                 return
 
-        message = update.message.reply_text(tipping)
-
-        if bot.get_chat(update.message.chat_id).type == Chat.PRIVATE:
-            remove_time = self.config.get("private_remove_after")
-        else:
-            remove_time = self.config.get("public_remove_after")
-
-        self.run_job(
-            self._remove_msg,
-            datetime.now() + timedelta(seconds=remove_time),
-            context=f"{message.chat_id}_{message.message_id}")
-
-    def _remove_msg(self, bot, job):
-        param_lst = job.context.split("_")
-        chat_id = param_lst[0]
-        msg_id = param_lst[1]
-
-        try:
-            logging.info(f"Removing {self.get_name()}-message (chat_id {chat_id} msg_id {msg_id})")
-            bot.delete_message(chat_id=chat_id, message_id=msg_id)
-            logging.info(f"Removed {self.get_name()}-message (chat_id {chat_id} msg_id {msg_id})")
-        except Exception as e:
-            msg = f"Not possible to remove {self.get_name()}-message (chat_id {chat_id} msg_id {msg_id}): {e}"
-            logging.error(msg)
+        update.message.reply_text(tipping)
