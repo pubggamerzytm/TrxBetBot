3a411f35b75b4304b8847228b9dad42510c5d13b
Endogen <081081@gmx.net>
2020-06-28 11:13:36 +0200

    Simplified error handling

--- trxbetbot/trongrid.py
+++ trxbetbot/trongrid.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import requests
 
 from requests.adapters import HTTPAdapter
@@ -23,13 +24,24 @@ class Trongrid:
             content = json.loads(response.content.decode("utf-8"))
             response.raise_for_status()
             return content
-        except Exception:
-            try:
-                raise ValueError(content)
-            except UnboundLocalError:
-                pass
-            raise
+        except Exception as e:
+            msg = f"Error calling URL {url}: {e}"
+            logging.error(msg)
+            raise e
+
+    def __url_params(self, api_url, params):
+        if params:
+            api_url += '?'
+            for key, value in params.items():
+                api_url += f"{key}={value}&"
+            api_url = api_url[:-1]
+        return api_url
 
     def get_account(self, address):
         api_url = f"{self.api_base_url}accounts/{address}"
         return self.__request(api_url)
+
+    # https://developers.tron.network/reference#transaction-information-by-account-address
+    def get_transactions(self, address, **kwargs):
+        api_url = f"{self.api_base_url}accounts/{address}/transactions"
+        return self.__request(self.__url_params(api_url, kwargs))
