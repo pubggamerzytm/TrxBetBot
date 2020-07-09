f53f4024b94a2f6149fe97438c1c1245efa06cd8
endogen <ramodroll>
2020-07-09 16:39:37 +0200

    Finished TRON API auto-change

--- trxbetbot/trxapi.py
+++ trxbetbot/trxapi.py
@@ -3,9 +3,10 @@ import random
 import logging
 import trxbetbot.constants as con
 
-from tronapi import Tron, constants
-from tronapi.manager import TronManager
+from tronapi import Tron
+from requests import Session
 from trxbetbot.config import ConfigManager as Cfg
+from requests.exceptions import ConnectionError, ReadTimeout
 
 
 class TRXAPI(Tron):
@@ -13,10 +14,9 @@ class TRXAPI(Tron):
     cfg = Cfg(os.path.join(con.DIR_CFG, con.FILE_CFG))
 
     def __init__(self, **kwargs):
-        self.kwargs = self.enrich(**kwargs)
-        super().__init__(**self.kwargs)
+        super().__init__(**self.enrich_kwargs(**kwargs))
 
-    def enrich(self, **kwargs):
+    def enrich_kwargs(self, **kwargs):
         if "full_node" not in kwargs:
             full_node = self.cfg.get("tron", "default_full_node")
             if full_node:
@@ -25,50 +25,99 @@ class TRXAPI(Tron):
             solidity_node = self.cfg.get("tron", "default_solidity_node")
             if solidity_node:
                 kwargs["solidity_node"] = solidity_node
-        if "event_server" not in kwargs:
-            event_server = self.cfg.get("tron", "default_event_node")
-            if event_server:
-                kwargs["event_server"] = event_server
 
         return kwargs
 
-    def _refresh(self):
-        self.kwargs.setdefault('full_node', constants.DEFAULT_NODES['full_node'])
-        self.kwargs.setdefault('solidity_node', constants.DEFAULT_NODES['solidity_node'])
-        self.kwargs.setdefault('event_server', constants.DEFAULT_NODES['event_server'])
+    def full_node_connected(self):
+        data = {
+            'method': 'get',
+            'url': f'{self.manager.full_node.node_url}/wallet/getnowblock',
+            'timeout': 1
+        }
 
-        self.manager = TronManager(self, dict(
-            full_node=self.kwargs.get('full_node'),
-            solidity_node=self.kwargs.get('solidity_node'),
-            event_server=self.kwargs.get('event_server')))
+        return self._node_connected(data)
 
-    def is_available(self):
+    def solidity_node_connected(self):
+        data = {
+            'method': 'get',
+            'url': f'{self.manager.solidity_node.node_url}/walletsolidity/getnowblock',
+            'timeout': 1
+        }
+
+        return self._node_connected(data)
+
+    def _node_connected(self, data):
         try:
-            status = self.is_connected()
-        except Exception as e:
-            logging.error(f"TRON API: Not possible to connect: {e}")
+            response = Session().request(**data)
+        except (ConnectionError, ReadTimeout):
             return False
-        return True if all(v is True for v in status.values()) else False
 
-    def find_server(self, retry=3):
+        if not (200 <= response.status_code < 300):
+            return False
+
+        try:
+            data = response.json()
+        except ValueError:
+            return False
+
+        if 'blockID' in data or data == 'OK':
+            return True
+
+        return False
+
+    def reconnect(self, fun, *args, **kwargs):
+        result = None
+
+        try:
+            return fun(*args, **kwargs)
+        except Exception as e:
+            logging.error(f"TRON API: Reconnecting {e} - Input: {fun} {args} {kwargs} - Result: {result}")
+
+            if not self.full_node_connected():
+                self.change_full_node()
+            if not self.solidity_node_connected():
+                self.change_solidity_node()
+
+            try:
+                return fun(*args, **kwargs)
+            except:
+                logging.error(f"TRON API: ERROR {e} - Input: {fun} {args} {kwargs} - Result: {result}")
+                return None
+
+    def change_full_node(self, retry=3):
         # Get server list from config
-        full_node_list = self.cfg.get("tron", "full_node_list")
-        solidity_node_list = self.cfg.get("tron", "solidity_node_list")
+        full_nodes = self.cfg.get("tron", "full_node_list")
 
-        current_host = self.kwargs["full_node"]
-
-        connected = False
         for i in range(retry):
             # Choose random server
-            new_host = random.choice(full_node_list)
-            if new_host is not current_host:
-                self.set_server(new_host)
-                if self.is_available():
-                    connected = True
-                    break
+            new_node = random.choice(full_nodes)
+            if new_node is not self.manager.full_node.node_url:
+                self.manager.full_node.node_url = new_node
+                if self.full_node_connected():
+                    self.cfg.set(new_node, "tron", "default_full_node")
+                    logging.info(f"TRON API: Changed Full Node to {new_node}")
+                    return
                 else:
-                    logging.error(f"TRON API: Host #{i} not available: {new_host}")
+                    logging.warning(f"TRON API: Full Node #{i+1} not available: {new_node}")
 
-        if not connected:
-            self.set_server()
-            logging.error("TRON API: Reset to default host")
+        self.manager.full_node.node_url = self.enrich_kwargs(**{})["full_node"]
+        logging.warning("TRON API: Reset Full Node to default")
+
+    def change_solidity_node(self, retry=3):
+        # Get server list from config
+        solidity_nodes = self.cfg.get("tron", "solidity_node_list")
+
+        for i in range(retry):
+            # Choose random server
+            new_node = random.choice(solidity_nodes)
+            if new_node is not self.manager.solidity_node.node_url:
+                self.manager.solidity_node.node_url = new_node
+                if self.solidity_node_connected():
+                    self.cfg.set(new_node, "tron", "default_solidity_node")
+                    logging.info(f"TRON API: Changed Solidity Node to {new_node}")
+                    return
+                else:
+                    logging.warning(f"TRON API: Solidity Node #{i+1} not available: {new_node}")
+
+        self.manager.full_node.node_url = self.enrich_kwargs(**{})["solidity_node"]
+        logging.warning("TRON API: Reset Solidity Node to default")
