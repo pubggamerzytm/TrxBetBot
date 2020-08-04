210ff9a916c6312e2e93b3250d053190f4b4c6d8
Endogen <081081@gmx.net>
2020-08-04 21:01:09 +0200

    Fixed /walletgen

--- trxbetbot/plugins/walletgen/walletgen.py
+++ trxbetbot/plugins/walletgen/walletgen.py
@@ -26,10 +26,12 @@ class Walletgen(TrxBetBotPlugin):
                 update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
                 return
 
-        sql = self.get_resource("count_addr_date.sql")
+        sql = self.get_resource("count_bets_date.sql")
         res_bet = self.execute_sql(sql, f"-{days} day", plugin="bet")
         res_mix = self.execute_sql(sql, f"-{days} day", plugin="mix")
         res_win = self.execute_sql(sql, f"-{days} day", plugin="win")
+
+        sql = self.get_resource("count_addr_date.sql")
         res_global = self.execute_global_sql(sql, f"-{days} day")
 
         # Historic data
@@ -41,7 +43,7 @@ class Walletgen(TrxBetBotPlugin):
         bet = res_bet['data'][0][0] if res_bet['data'] else 0
         mix = res_mix['data'][0][0] if res_mix['data'] else 0
         win = res_win['data'][0][0] if res_win['data'] else 0
-        usr = res_global['data'][0][0] if res_global['data'] else 0  # FIXME: There is no table 'bets' here
+        usr = res_global['data'][0][0] if res_global['data'] else 0
         total = bet + mix + win + usr
 
         msg = f"*Generated addresses in last {days} days*\n\n" \
